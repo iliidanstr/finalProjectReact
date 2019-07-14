ae4a139af9c50f225eb0eefdbcdcbb8dd8d48fb7
iliidanstr <mwildan.elektro09@gmail.com>
2019-07-15 01:03:26 +0700

    Final Project

---
+++ src/data.js
@@ -0,0 +1,116 @@
+/* 
+https://www.iconfinder.com/icons/3643758/gadget_handphone_mobile_phone_smartphone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixel 3",
+    img: "img/product-1.png",
+    price: 600,
+    company: "GOOGLE",
+    info:
+      "The Google Pixel 3 mobile features a 5.5 (13.97 cm) display with a screen resolution of 1080 x 2160 pixels and runs on Android v9.0 (Pie) operating system. The device is powered by Octa core (2.5 GHz, Quad core, Kryo 385 + 1.6 GHz, Quad core, Kryo 385) processor paired with 4 GB of RAM. As far as the battery is concerned it has 2915 mAh. Over that, as far as the rear camera is concerned this mobile has a 12.2 MP camera CMOS image sensor supporting a resolution of 4032 x 3024 Pixels and the front snapper is powered by a CMOS image sensor. Other sensors include Light sensor, Proximity sensor, Accelerometer, Barometer, Compass, Gyroscope. So, does it have a fingerprint sensor? Yes, it does. For graphical performance that can make games run smoothly, this phone has got a Adreno 630 GPU. On board storage is at 64 GB with the option to expand the memory by No. Design is one of the most important factors when it comes to mobiles. This phone is 7.9 mm slim and weighs 148 grams.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung S10",
+    img: "img/product-2.png",
+    price: 580,
+    company: "SAMSUNG",
+    info:
+      "Body: Aluminum frame, Gorilla Glass 6 front, GG5 back, 149.9x70.4x7.8mm, 157g; Prism White, Prism Black, Prism Green, Prism Blue, Canary Yellow, Flamingo Pink color schemes. IP68 rating for dust and water protection.Display: 6.1 Infinity-O Dynamic AMOLED, 1,440x3,040px, 19:9 (2.11:1) aspect ratio, 550ppi; HDR10+ support (first).Rear camera: Wide (main): 12MP, 1/2.55 sensor, f/1.5-2.4 aperture, 26mm equiv. focal length (77° FoV), dual pixel PDAF, OIS. Telephoto: 12MP, 1/3.6 sensor, f/2.4 aperture, 52mm equiv. focal length (45° FoV), PDAF, OIS. Ultra-wide: 16MP, f/2.2 aperture, 12mm equiv. focal length (123° FoV), fixed focus.Front camera: 10MP, f/1.9 aperture, 25mm equiv. focal length (80° FoV), dual pixel PDAF.Video recording: Rear: up to 4K 2160p@60fps, EIS up to 2160p@30fps, slow-mo up to 1080p@240fps, super slow-mo 720p@960fps for up to 0.4s (12s playback at normal speed); HDR10+ recording. Front: up to 4K 2160p@30fps with EIS.OS/Software: Android 9.0 Pie, Samsung One UI.Chipset (market dependent): Snapdragon 855 (7nm): octa-core CPU (1x2.8GHz & 3x2.4GHz Kryo Gold & 4x1.7GHz Kryo 485 Silver); Adreno 640 GPU. Exynos 9820 (8nm): octa-core CPU (2x2.7GHz Mongoose M4 & 2x2.3GHz Cortex-A75 & 4x1.9GHz Cortex-A55); Mali-G76 MP12 GPU.Memory: 8GB RAM with 128GB storage, 8GB RAM with 512GB storage; microSD card slot (hybrid on the dual SIM versions).Battery: 3,400mAh Li-Ion (sealed), 15W wired charging (Adaptive Fast charging, QuickCharge 2.0 compatible), 15W Fast Wireless Charging 2.0, Wireless PowerShare.Connectivity: Single-SIM, Dual-SIM available in certain markets (hybrid slot); LTE-A, 7-Band carrier aggregation, Cat.20/13 (2Gbps/150Mbps); USB Type-C (v3.1); Wi-Fi a/b/g/n/ac/ax MU-MIMO; GPS, GLONASS, Beidou, Galileo; NFC; Bluetooth 5.0.Misc: Ultrasonic under-display fingerprint sensor; stereo speakers (bottom-firing + earpiece); bundled AKG headphones.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "OnePlus 7",
+    img: "img/product-3.png",
+    price: 640,
+    company: "OnePlus",
+    info:
+      "Body: 157.7 x 74.8 x 8.2 mm, 182 grams, Gorilla Glass 6 front and Gorilla Glass 5 back, metal side frame.Screen: 6.41 Optic AMOLED, 1080 x 2340px resolution; 19.5:9 aspect ratio; 402ppi.Chipset: Qualcomm SDM855 Snapdragon 855 (7 nm); Octa-core (1x2.84 GHz Kryo 485 & 3x2.42 GHz Kryo 485 & 4x1.80 GHz Kryo 485); Adreno 640.Memory: 6GB RAM, 128GB built-in storage.OS: OxygenOS based on Android 9.0 Pie.Rear camera: Main: 48MP, f/1.7, 1/2, 0.8µm pixel size; PDAF; OIS; 5MP depth sensor; 2160p@30/60fps video recording.Front camera: 16MP, f/2.0; 1/3.1, 1.0µm; 1080p@30fps video recording.Battery: 3,700 mAh, 20W fast charging.Misc: Alert slider, stereo loudspeakers, NFC, under-display fingerprint reader, UFS 3.0 storage, USB-C 3.1 connector, available in Mirror Gray and Red.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Huawei P30 Pro",
+    img: "img/product-4.png",
+    price: 1140,
+    company: "Huawei",
+    info:
+      " Body: dual-glass with metal frame; IP68-rated for dust and water resistance.Screen: 6.47 AMOLED, 1,080 x 2,340 px resolution (398ppi); tiny notch; acoustic display.Chipset: Kirin 980 chipset, octa-core processor (2xA76 @2.6GHz + 2xA76 @1.92GHz +4xA55 @1.8GHz), Mali-G76 MP10 GPU, graphene film and heat pipe with vapor chamber cooling system.Memory: 6/8GB RAM, 128/256/512GB storage (expandable via Nano Memory - hybrid slot).OS: Android 9 Pie with EMUI 9.1;.Camera: 40MP f/1.6 OIS (RYYB filter) + 8MP with periscope 135mm f/3.4 OIS lens (5x optical zoom) + 20MP f/2.2 ultra-wide (16mm) + ToF camera; 4K video capture, 720@960fps slow-mo, Leica branding.Camera features: 1/1.7 40MP sensor, up to ISO 204,800, 5x optical zoom, 10x hybrid zoom, 50x digital zoom, Super Macro Mode, OIS + EIS, Variable Aperture, Portrait Mode, Night Mode.Selfie cam: 32MP, f/2.0 Leica lens, Portrait Mode with live bokeh effects.Battery: 4,200mAh; Super Charge 40W; 15W wireless charging; reverse wireless charging.Security: Fingerprint reader (under display).Connectivity: Dual SIM, Wi-Fi a/b/g/n/ac, Dual-band GPS; Bluetooth 5 + LE, NFC, USB Type-C.Misc: IR blaster, acoustic display acts as earpiece, bottom-firing loudspeaker .",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "LG G7",
+    img: "img/product-5.png",
+    price: 400,
+    company: "LG",
+    info:
+      "Body: Aluminum frame with Gorilla Glass 5 on front and rear; MIL-STD-810G compliant; IP 68 water-resistant screen: 6.1 QHD+ FullVision MLCD+ RGBW; 19.5:9 aspect ratio with notch, 564ppi; HDR 10 supported; brightness up to 1000 nits,Camera: 16MP (f/1.6) primary camera with OIS, Phase detection + Laser autofocus; secondary wide camera with 107-degree FOV, no OIS, fixed focus; Max 2160p @ 60fps video recording for both.Selfie cam: 8MP, 90-degree FOV; 1080p @ 30fps recording.Chipset: Qualcomm Snapdragon 845: octa-core CPU (4x2.8 GHz Kryo 385 Gold & 4x1.7 GHz Kryo 385 Silver), Adreno 630 GPU,Memory: 6GB of RAM; 128GB storage; microSD slot.OS: Android 8.0 Oreo with LG UX, Android P update expected,Battery: 3,000mAH Lithium Polymer (sealed); Quick Charge 3.0 fast charging; WPC&PMA Wireless charging; Qnovo battery tech.Connectivity: Single-SIM, Dual-SIM available in certain markets; LTE-A, 3-Band carrier aggregation, Cat.16/13 (1Gbps/150Mbps); USB Type-C; Wi-Fi a/b/g/n/ac; GPS; Bluetooth 5.0; FM radio,Misc: Fingerprint reader; Hi-Fi Quad DAC via 3.5mm headphone jack; 2 mics, Boombox speaker",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Oppo Reno 10x",
+    img: "img/product-6.png",
+    price: 563,
+    company: "Oppo",
+    info:
+      "The Oppo Reno 10x was great camera and awesome features, 6/8GB RAM, 128/256GB storage, video recording up to 4K 2160p@60fps, EIS up to 2160p@30fps, slow-mo up to 1080p@240fps. Front: up to 1080p@30fps, OS/Software Android 9.0 Pie, ColorOS 6. 6.6 AMOLED, 1080x2340px, 19.5:9 aspect ratio, 387ppi display.The rest of the Reno is standard as flagships go these days – a Snapdragon 855 chipset with lots of RAM and storage, a 4,000+ mAh battery with VOOC fast charging, optical under-display fingerprint reader (spoiler alert: it’s blazing fast!), stereo speakers, and Android Pie with the newest Color launcher.The Oppo Reno 10x zoom has a motorized pop-up selfie camera that makes it pretty much impossible to secure against water. Not that Oppo had IP-rated phones before that, but in case you were wondering – the Reno is neither water, nor dust protected officially. The teardowns are yet to reveal if there are some ingress sealings as was the case with the OnePlus 7, though we still would keep the phone away from water just in case.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Samsung Galaxy M10",
+    img: "img/product-7.png",
+    price: 130,
+    company: "Samsung",
+    info:
+      "Samsung Galaxy M10 Android smartphone. Announced Jan 2019. Features 6.22 PLS TFT display, Exynos 7870 Octa chipset, 3400 mAh battery, 32 GB storage, 3 GB RAM.Samsung Galaxy M10 32GB mobile features a 6.22 (15.8 cm) display with a screen resolution of 720 x 1520 pixels and runs on Android v8.1 (Oreo) operating system. The device is powered by Octa core, 1.6 GHz, Cortex A53 processor paired with 3 GB of RAM. As far as the battery is concerned it has 3400 mAh. Over that, as far as the rear camera is concerned this mobile has a 13 MP + 5 MP camera . Other sensors include Light sensor, Proximity sensor, Accelerometer. So, does it have a fingerprint sensor? No, it doesn’t. For graphical performance that can make games run smoothly, this phone has got a Mali-T830 GPU.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Iphone XR-Black",
+    img: "img/product-10.png",
+    price: 1499,
+    company: "Apple",
+    info:
+      "Body: Aluminum frame, glass back, IP67-certified, six colors.Display: 6.1-inch, 1792 x 828 (326 PPI), IPS LCD, 1400:1, wide-color (P3).Rear camera: 12MP, f1.8, OIS + EIS, dual pixel PDAF, quad-LED flash, 4K60 video, wide-color.Front camera: 7MP, f2.2, 1080p60, EIS, display flash, wide-color.Software: iOS 12.Chipset: Apple 12 Bionic, 2.49GHz six-core CPU (2x Vortex + 4x Tempest), quad-core Apple GPU, octa-core Neural Engine.Memory: 3GB LPDDR4X SDRAM, 64/128/256GB NVMe storage.Connectivity: Dual SIM (nano SIM + eSIM), LTE-A, dual-band Wi-Fi 802.11ac with 2x2 MIMO, Bluetooth 5.0, AGPS/GLONASS/Galileo/QZSS, NFC (Apple Pay only).Battery: 2942mAh, USB-PD fast charge support, Qi charging support, 5W supplied charger.Misc: TrueDepth camera system with Face ID, stereo speakers",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel 3",
+  img: "img/product-1.png",
+  price: 600,
+  company: "google",
+  info:
+    "Google Pixel smartphone was launched in October 2016. The phone comes with a 5.00-inch touchscreen display with a resolution of 1080x1920 pixels at a pixel density of 441 pixels per inch (ppi).Google Pixel is powered by a 1.6GHz quad-core Qualcomm Snapdragon 821 processor. It comes with 4GB of RAM.The Google Pixel runs Android 7.1 and is powered by a 2,770mAh non-removable battery.As far as the cameras are concerned, the Google Pixel on the rear packs 12.3-megapixel camera. It sports a 8-megapixel camera on the front for selfies.Google Pixel based on Android 7.1 and packs 32GB of inbuilt storage. The Google Pixel is a single SIM (GSM) smartphone that accepts a Nano-SIM card.Connectivity options on the Google Pixel include Wi-Fi 802.11 a/b/g/n/ac, GPS, Bluetooth v4.20, NFC, 3G, and 4G (with support for Band 40 used by some LTE networks in India). Sensors on the phone include accelerometer, ambient light sensor, barometer, gyroscope, proximity sensor, temperature sensor, and compass/ magnetometer.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
